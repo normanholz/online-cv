368afe73c50c2098801f3f6ebf6409bac2e9c116
Norman Holz <normanholz@gmail.com>
2023-09-01 14:19:39 +0200

    added more data

---
+++ pdf/generate_latex.rb
@@ -0,0 +1,45 @@
+require 'yaml'
+require 'erb'
+
+def generate_latex(yaml_file, template_file, output_file)
+    # Load data from YAML file
+    yaml_data = YAML.load_file(yaml_file)
+
+    # Load ERB template
+    template = File.read(template_file)
+    erb = ERB.new(template)
+
+    binding_obj = binding()
+
+    sidebar_data = yaml_data['sidebar']
+    binding_obj.local_variable_set(:name, sidebar_data['name'])
+    binding_obj.local_variable_set(:email, sidebar_data['email'])
+    binding_obj.local_variable_set(:avatar, sidebar_data['avatar'])
+
+    cv_data = sidebar_data['cv']
+    binding_obj.local_variable_set(:phone, cv_data['phone'])
+    binding_obj.local_variable_set(:address, cv_data['address'])
+
+
+    # Experience
+    experiences_data = yaml_data['experiences']
+    binding_obj.local_variable_set(:experiences_title, experiences_data['title'])
+    binding_obj.local_variable_set(:experiences_data, experiences_data['info'])
+
+    # Education
+    education_data = yaml_data['education']
+    binding_obj.local_variable_set(:education_title, education_data['title'])
+    binding_obj.local_variable_set(:education_data, education_data['info'])
+
+    # Languages
+    binding_obj.local_variable_set(:languages_data, sidebar_data['languages']['info'])
+
+
+    # Render the template with YAML data
+    latex_content = erb.result(binding_obj)
+
+    # Write generated LaTeX content to output file
+    File.write(output_file, latex_content)
+end
+
+generate_latex('./_data/data.yml', './pdf/cv-template.erb', './pdf/output.tex')
